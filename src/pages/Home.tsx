24dfc1352693a8a44a15d714bc7979098c5e8478
Syed Ali Abbas <aliabbasshah72@gmail.com>
2025-08-03 01:07:04 +0500

    img update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,7 +39,7 @@ const Home: React.FC = () => {
             <Box display="flex" flexDirection="column" alignItems="center">
               <Avatar
                 alt="Syed Ali Abbas"
-                src="images/profile.jpeg"
+                src="images/profile.jpg"
                 sx={{
                   width: 300,
                   height: 300,
