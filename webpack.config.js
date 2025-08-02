2403ee6908e710728893bf86eb65a836fed07c15
Syed Ali Abbas <90503400+aliabbas512@users.noreply.github.com>
2025-08-02 21:49:48 +0500

    myportfoliowebsite v1
    initial files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: './public',
+    historyApiFallback: true,
+    port: 3000,
+  },
+};
